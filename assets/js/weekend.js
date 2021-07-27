bacba47d1670258acc4c8c494c6094ea4693240f
unknown <boedi15@gmail.com>
2021-07-27 02:10:22 -0600

    added drop down menus

--- assets/js/weekend.js
+++ assets/js/weekend.js
@@ -1,3 +1,40 @@
+var subjectObject = {
+    "Front-end": {
+      "HTML": ["Links", "Images", "Tables", "Lists"],
+      "CSS": ["Borders", "Margins", "Backgrounds", "Float"],
+      "JavaScript": ["Variables", "Operators", "Functions", "Conditions"]    
+    },
+    "Back-end": {
+      "PHP": ["Variables", "Strings", "Arrays"],
+      "SQL": ["SELECT", "UPDATE", "DELETE"]
+    }
+  }
+  window.onload = function() {
+    var subjectSel = document.getElementById("subject");
+    var topicSel = document.getElementById("topic");
+    var chapterSel = document.getElementById("chapter");
+    for (var x in subjectObject) {
+      subjectSel.options[subjectSel.options.length] = new Option(x, x);
+    }
+    subjectSel.onchange = function() {
+      //empty Chapters- and Topics- dropdowns
+      chapterSel.length = 1;
+      topicSel.length = 1;
+      //display correct values
+      for (var y in subjectObject[this.value]) {
+        topicSel.options[topicSel.options.length] = new Option(y, y);
+      }
+    }
+    topicSel.onchange = function() {
+      //empty Chapters dropdown
+      chapterSel.length = 1;
+      //display correct values
+      var z = subjectObject[subjectSel.value][this.value];
+      for (var i = 0; i < z.length; i++) {
+        chapterSel.options[chapterSel.options.length] = new Option(z[i], z[i]);
+      }
+    }
+  }
 // hardcoded lat and lon will later be passed from map API
 var lat = parseInt("40.7608");
 var lon = parseInt("-111.8911");
@@ -47,3 +84,4 @@ var displayDailyWeather = function (data) {
 // calls getDailyWeather function to run current weather api with hard coded value of Salt Lake City
 // will be changed to an event listener capturing the value of the user input city name
 getDailyWeather(lat, lon);
+
