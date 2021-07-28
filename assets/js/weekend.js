b0ff4e60d8c608ad98612e8ab1d1c80661f98639
Sean <sdillon215@gmail.com>
2021-07-27 19:10:38 -0600

    displayDailyWeather edit

--- assets/js/weekend.js
+++ assets/js/weekend.js
@@ -23,18 +23,16 @@ var displayDailyWeather = function (data) {
         var dailyDate = new Date(millisecond);
         var dailyDate = dailyDate.toLocaleString("en-US", options);
 
-        var cardList =$("<div>").addClass("cell");
-        var weatherList = $("<div>").addClass("card");
+        var weatherList = $("<div>").addClass("columns");
         // need to add classes to style weather list ex: var cityname = $("<li>").addClass("new classes here").text(data.name);
-        var date = $("<div>").addClass("card-section").text(dailyDate);
-        var cityTemp = $("<div>").addClass("card-section").text("Tempurature: " + data.daily[i].temp.day + "F");
-        var cityWind = $("<div>").addClass("card-section").text("Wind: " + data.daily[i].wind_speed + " Mph");
-        var cityHumid = $("<div>").addClass("card-section").text("Humidity: " + data.daily[i].humidity + "%");
-        var cityImage = $("<img>").addClass("card-section").attr("src", "https://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png");
+        var date = $("<div>").text(dailyDate);
+        var cityTemp = $("<div>").text("Tempurature: " + data.daily[i].temp.day + "F");
+        var cityWind = $("<div>").text("Wind: " + data.daily[i].wind_speed + " Mph");
+        var cityHumid = $("<div>").text("Humidity: " + data.daily[i].humidity + "%");
+        var cityImage = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png");
         // appends weather info to <ul> as <li>
         weatherList.append(date, cityTemp, cityWind, cityHumid, cityImage);
         // appends <ul> to div with the id of weather
-        cardList.append(weatherList);
-        $("#weather").append(cardList);
+        $("#weather").append(weatherList);
     };
 };
