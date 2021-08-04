09d768d82bd07961abf7151a7d8684708a122918
Sean <sdillon215@gmail.com>
2021-08-04 13:06:29 -0600

    cityArr set to LS

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -3,6 +3,7 @@ var cityArr = JSON.parse(localStorage.getItem("cityArr")) || [];
 var loadCity = function(city) {
     if (cityArr.indexOf(city) === -1) {
         cityArr.push(city);
+        localStorage.setItem("cityArr", JSON.stringify(cityArr));
     }
 console.log(cityArr);
 };
